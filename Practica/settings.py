d63da67dda5d4ba99f32b11a3ce231d9b2149623
darck809 <dany8mt@gmail.com>
2024-07-10 23:32:36 -0500

    estilos añadidos

--- Practica/settings.py
+++ Practica/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!a^h449temjp+n-v_rf^t!wwbm@m08^f50z0es85=+(w8nx+l%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'proyect',
 ]
 
 MIDDLEWARE = [
--- proyect/models.py
+++ proyect/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+#aqui se crea las tablas de la base de datos
 # Create your models here.
+class Project(models.Model):
+    name: models.CharField(max_length=200)
+    firstname = models.CharField(max_length=255)
